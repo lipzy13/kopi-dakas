dc85a67663bd5996b6f706f71aa63f98688a0e75
lipzy13 <88047810+lipzy13@users.noreply.github.com>
2024-12-07 22:50:01 +0700

    Update package.json
    Update package.json
    
    Update package.json
    
    Update package.json
    
    Update tailwind.config.js
    
    Create vercel.json

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+import tailwindcssAnimate from "tailwindcss-animate";
 /** @type {import('tailwindcss').Config} */
 export default {
     darkMode: ["class"],
@@ -63,6 +64,6 @@ export default {
   		}
   	}
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [tailwindcssAnimate],
 }
 
